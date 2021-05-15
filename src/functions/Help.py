dd002547518be9be1d0e82b93c3cc14015c3af0a
Antonio Muniz <antonio.gustavo_@live.com>
2021-05-15 19:31:54 -0300

    Create help response

--- src/functions/Help.py
+++ src/functions/Help.py
@@ -1,4 +1,17 @@
 class Help:
     @staticmethod
     def response(connectionSocket,  server) -> None:
-        print(connectionSocket)
+        message = ("\nAvailable commands\n" +
+                "\n/help" +
+                "\n/register -name -pass -nick" +
+                "\n/login  -nick -pass " +
+                "\n/create -room_name -size" +
+                "\n/join -room_name" +
+                "\n//m //-m || //message // -message" +
+                "\n//send -path" +
+                "\n/leave" +
+                "\n/list" +
+                "\n/list_files" +
+                "\n/list_users" + 
+                "\n/quit\n\n")
+        connectionSocket.send(message.encode())
