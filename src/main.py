6cd11341932ecd62ec23bd97f82a53acab6e2bb5
João Vitor Dias <joao.dias.santos223@gmail.com>
2021-05-26 20:53:32 -0300

    style(): delete unused imports
    Signed-off-by: João Vitor Dias <joao.dias.santos223@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,10 +1,4 @@
 from entities.Server import startServer
-from repositories.MessageRepository import MessageRepository
-from repositories.RoomRepository import RoomRepository
-from repositories.UserRepository import UserRepository
-from src.entities.dtos.User import User
-from src.entities.dtos.Room import Room
-from src.entities.dtos.Message import Message
 
 
 def main() -> None:
